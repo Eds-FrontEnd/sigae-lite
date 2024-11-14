60434fe6198eeaa810ddeabfc7fac3311de5c9e0
Edson-Dev <edsfrontend10@gmail.com>
2024-11-13 22:55:01 -0300

    Removido a customização de carregamento de página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,14 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
-import { LogoComponent } from './components/logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  encapsulation:
-  ViewEncapsulation.None,
   standalone: true,
-  imports: [ RouterOutlet, MatIconModule, CommonModule, LogoComponent ],
+  imports: [ RouterOutlet, MatIconModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'sigae';
-  loading: boolean = true;
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.loading = false;
-    }, 1500);
-  }
 }
